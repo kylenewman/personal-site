5675612e88d71225ddef164a3a052d3048d2abc6
kylenewman <kndesign@yahoo.com>
2016-04-11 22:28:15 -0500

    add alt and title attributes

---
+++ js/bigSlideCall.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+          $('.menu-link').bigSlide({
+            'side': 'right',
+            'menuWidth': '14em',
+            'speed': '800',
+          });
+      });
